9a3d961f850d8412018d22f73b022fea474f500a
TinyUniverseDevelopment <humanc0ntact0@gmail.com>
2018-02-11 17:44:26 -0500

    routes and models added for sign up

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const app = express()
-
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
-app.get('/posts', (req, res) => {
-  res.send(
-    [{
-      title: "Hello World!",
-      description: "Hi there! How are you?"
-    }]
-  )
-})
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')('session');
+const app = express();
 
 // connect to db
-
-
-app.listen(process.env.PORT || 8081)
 
 const db = 'mongodb://master:password@ds117758.mlab.com:17758/doverusers';
 mongoose.connect(db);
 const conn = mongoose.connection;
 conn.once('open', () => console.log('connected'));
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(session({
+  secret: 'opensesame',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+app.listen(process.env.PORT || 8081);
