30e9ee8cf00551ff0965f81268b42d0c7ca6d02e
bbuccianti <bebuccianti@gmail.com>
2018-02-16 13:47:20 -0300

    get projects by userId

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.use(authenticate);
 
 router.get('/', (req, res) => {
-  res.json({message: 'it works!!'});
+  Project.find({ userId: req.currentUser._id }).then(projects => res.json({ projects }));
 });
 
 router.post('/', (req, res) => {
