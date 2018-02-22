34332060e5489bf76ce6efaa9c2b6904e474adcf
TinyUniverseDevelopment <humanc0ntact0@gmail.com>
2018-02-22 11:55:02 -0500

    updated routes for bio edit

--- src/models/bio.js
+++ src/models/bio.js
@@ -33,7 +33,7 @@ const schema = new mongoose.Schema({
     type: String,
     required: false,
   },
-  imgSrc: {
+  linkedinUsername: {
     type: String,
     required: false,
   },
--- src/routes/bio.js
+++ src/routes/bio.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongo from 'mongodb';
+import assert from 'assert';
 import authenticate from '../middlewares/authenticate';
 import Bio from '../models/bio';
 
@@ -11,8 +13,8 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   const userId = req.currentUser;
-  const { firstName, lastName, companyName, githubUsername, twitterUsername, about, portfolioLink } = req.body.userinfo;
-  const userinfo = new Bio({ firstName, lastName, companyName, githubUsername, twitterUsername, about, portfolioLink, userId });
+  const { firstName, lastName, companyName, githubUsername, twitterUsername, about, portfolioLink, linkedinUsername } = req.body.userinfo;
+  const userinfo = new Bio({ firstName, lastName, companyName, githubUsername, twitterUsername, about, portfolioLink, linkedinUsername, userId });
 
   userinfo.save().then(userinfo => {
     res.json({ userinfo });
@@ -20,5 +22,26 @@ router.post('/', (req, res) => {
     res.status(400).json({ errors: err });
   })
 });
+
+router.put('/edit', (req, res) => {
+  const updatedInfo = {
+    firstName: req.body.userinfo.firstName,
+    lastName: req.body.userinfo.lastName,
+    companyName: req.body.userinfo.companyName,
+    about: req.body.userinfo.about,
+    githubUsername: req.body.userinfo.githubUsername,
+    twitterUsername: req.body.userinfo.twitterUsername,
+    portfolioLink: req.body.userinfo.portfolioLink,
+    linkedinUsername: req.body.userinfo.linkedinUsername,
+
+  };
+  mongo.connect(process.env.MONGODB_URL, (err, database) => {
+    const db = database.db('doverusers');
+    db.collection('bios').findOneAndUpdate({userId: req.currentUser._id},{$set: updatedInfo});
+  })
+
+});
+
+
 
 export default router;
